4e72ff46561888e6dbf86537b4dc18538c9280f3
Kim youngwoo <keiprogramo@gmail.com>
2024-05-11 22:44:04 +0900

    ✨feat : [LTC] 3137_Minimum Number of Operations to Make Word K-Periodic 문제 알고리즘 구현

---
+++ Gombang/Weekly Contest 396/3137_MinimumOperationsToMakeKPeriodic.cs
@@ -0,0 +1,29 @@
+﻿public class Solution
+{
+    public int MinimumOperationsToMakeKPeriodic(string word, int k)
+    {
+        Dictionary<string, int> splitDic = new Dictionary<string, int>();
+        for (int i = 0; i < word.Length; i += k)
+        {
+            string splitWord = word.Substring(i, k);
+
+            if (splitDic.ContainsKey(splitWord))
+            {
+                splitDic[splitWord]++;
+            }
+            else
+            {
+                splitDic[splitWord] = 1;
+            }
+        }
+
+        int maxCount = 0;
+        foreach (var keyValuePair in splitDic)
+        {
+            if (maxCount < keyValuePair.Value)
+                maxCount = keyValuePair.Value;
+        }
+
+        return (word.Length / k) - maxCount;
+    }
+}
