8cf074a0991341c51602159ef2ac7b21df1a919c
Kim youngwoo <keiprogramo@gmail.com>
2024-05-17 11:33:41 +0900

    [LTC] 3136_Valid_Word 알고리즘 문제 구현\n [LTC] 3137_Minimum_Operations_To_Make_K_Periodic 알고리즘 문제 구현

--- Gombang/Weekly Contest 396/3136_Valid_Word.cs
+++ Gombang/Weekly Contest 396/3136_Valid_Word.cs
@@ -1,50 +1,41 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿public class Solution
+{
+    char[] vowelArray = new char[] { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' };
 
-//namespace algorhtim
-//{
-//    public class Solution
-//    {
-//        char[] vowelArray = new char[] { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' };
+    public bool IsValid(string word)
+    {
+        if (word.Length < 3)
+            return false;
 
-//        public bool IsValid(string word)
-//        {
-//            if (word.Length < 3)
-//                return false;
+        bool hasVowel = false;
+        bool hasConsonant = false;
 
-//            bool hasVowel = false;
-//            bool hasConsonant = false;
+        for (int i = 0; i < word.Length; i++)
+        {
+            bool isLetter = char.IsLetter(word[i]);
+            bool isDigit = char.IsDigit(word[i]);
 
-//            for (int i = 0; i < word.Length; i++)
-//            {
-//                bool isLetter = char.IsLetter(word[i]);
-//                bool isDigit = char.IsDigit(word[i]);
+            if (!(isLetter || isDigit))
+                return false;
 
-//                if (!(isLetter || isDigit))
-//                    return false;
+            if (isLetter)
+            {
+                // 모음이 포함되어 있는지 체크.
+                if (vowelArray.Contains(word[i]))
+                {
+                    hasVowel = true;
+                    continue;
+                }
 
-//                if (isLetter)
-//                {
-//                    // 모음이 포함되어 있는지 체크.
-//                    if (vowelArray.Contains(word[i]))
-//                    {
-//                        hasVowel = true;
-//                        continue;
-//                    }
+                // 자음이 포함되어 있는지 체크.
+                if (word[i] >= 'a' && word[i] <= 'z' ||
+                    word[i] >= 'A' && word[i] <= 'Z')
+                {
+                    hasConsonant = true;
+                }
+            }
+        }
 
-//                    // 자음이 포함되어 있는지 체크.
-//                    if (word[i] >= 'a' && word[i] <= 'z' ||
-//                        word[i] >= 'A' && word[i] <= 'Z')
-//                    {
-//                        hasConsonant = true;
-//                    }
-//                }
-//            }
-
-//            return hasVowel && hasConsonant;
-//        }
-//    }
-//}
+        return hasVowel && hasConsonant;
+    }
+}
--- Gombang/Weekly Contest 396/3137_Minimum_Operations_To_Make_K_Periodic.cs
+++ Gombang/Weekly Contest 396/3137_Minimum_Operations_To_Make_K_Periodic.cs
@@ -1,29 +1,29 @@
-﻿//public class Solution
-//{
-//    public int MinimumOperationsToMakeKPeriodic(string word, int k)
-//    {
-//        Dictionary<string, int> splitDic = new Dictionary<string, int>();
-//        for (int i = 0; i < word.Length; i += k)
-//        {
-//            string splitWord = word.Substring(i, k);
+﻿public class Solution
+{
+    public int MinimumOperationsToMakeKPeriodic(string word, int k)
+    {
+        Dictionary<string, int> splitDic = new Dictionary<string, int>();
+        for (int i = 0; i < word.Length; i += k)
+        {
+            string splitWord = word.Substring(i, k);
 
-//            if (splitDic.ContainsKey(splitWord))
-//            {
-//                splitDic[splitWord]++;
-//            }
-//            else
-//            {
-//                splitDic[splitWord] = 1;
-//            }
-//        }
+            if (splitDic.ContainsKey(splitWord))
+            {
+                splitDic[splitWord]++;
+            }
+            else
+            {
+                splitDic[splitWord] = 1;
+            }
+        }
 
-//        int maxCount = 0;
-//        foreach (var keyValuePair in splitDic)
-//        {
-//            if (maxCount < keyValuePair.Value)
-//                maxCount = keyValuePair.Value;
-//        }
+        int maxCount = 0;
+        foreach (var keyValuePair in splitDic)
+        {
+            if (maxCount < keyValuePair.Value)
+                maxCount = keyValuePair.Value;
+        }
 
-//        return (word.Length / k) - maxCount;
-//    }
-//}
+        return (word.Length / k) - maxCount;
+    }
+}
