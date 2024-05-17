4804e8734a9a97ab225444786f98815bd174da75
Kim youngwoo <keiprogramo@gmail.com>
2024-05-17 11:11:02 +0900

    [LTC] 3146_Permutation_Difference_between_Two_Strings 문제 알고리즘 구현 [LTC] 3147_Taking_Maximum_Energy_From_the_Mystic_Dungeon 문제 알고리즘 구현

---
+++ Gombang/Weekly Contest 397/3147_Taking_Maximum_Energy_From_the_Mystic_Dungeon.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+
+public class Solution
+{
+    public int MaximumEnergy(int[] energy, int k)
+    {
+        int max = int.MinValue;
+        int[] maxEnergyArray = new int[energy.Length];
+        for (int i = energy.Length - 1; i >= 0; i--) {
+
+            if (i + k > energy.Length - 1)
+            {
+                maxEnergyArray[i] = energy[i];
+            }
+            else
+            {
+                maxEnergyArray[i] = maxEnergyArray[i + k] + energy[i];
+            }
+
+            max = Math.Max(max, maxEnergyArray[i]);
+        }
+
+        return max;
+    }
+}
