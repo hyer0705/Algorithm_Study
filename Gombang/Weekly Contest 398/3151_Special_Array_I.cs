a9935c3c2c1dc4cf6f8e00a0ac0b0b7561179d9b
Kim youngwoo <keiprogramo@gmail.com>
2024-05-24 14:04:46 +0900

    [LTC] 3151_Special_Array_I, 3152_Special_Array_II 알고리즘 문제 구현

---
+++ Gombang/Weekly Contest 398/3151_Special_Array_I.cs
@@ -0,0 +1,19 @@
+﻿// 인접한 인덱스에 대해서 나머지 연산을 하여 서로 같은 경우가 발생한다면 return false가 진행되도록 하였습니다.
+// 특별한 어려움은 없었습니다.
+
+public class Solution
+{
+    public bool IsArraySpecial(int[] nums)
+    {
+        if (nums.Length < 2)
+            return true;
+
+        for (int i = 0; i < nums.Length - 1; i++)
+        {
+            if (nums[i] % 2 == nums[i + 1] % 2)
+                return false;
+        }
+
+        return true;
+    }
+}
