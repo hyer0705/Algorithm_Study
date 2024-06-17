f466500cebe519c5fd4511a943b338d2c10a157b
Kim youngwoo <keiprogramo@gmail.com>
2024-06-17 10:55:21 +0900

    ✨feat : [LTC] 3179_Find_the_N-th_Value_After_K_Seconds 문제 알고리즘 구현

---
+++ Gombang/Weekly Contest 401/3179_Find_the_N-th_Value_After_K_Seconds.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Solution
+{
+    public int ValueAfterKSeconds(int n, int k)
+    {
+        // 10의 9승 + 7 표현.
+        int modulo = (int)(Math.Pow(10, 9) + 7);
+
+        int[] array = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            array[i] = 1;
+        }
+
+        for (int second = 0; second < k; second++)
+        {
+            for (int i = 1; i < n; i++)
+            {
+                array[i] = (array[i - 1] + array[i]) % modulo;
+            }
+        }
+
+        return array[n - 1];
+    }
+}
