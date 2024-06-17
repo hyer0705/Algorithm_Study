4fb350ef68c559b3cc6ceee4f9b4e166764f5574
Kim youngwoo <keiprogramo@gmail.com>
2024-06-17 10:23:52 +0900

    ✨feat : [LTC] 3178_Find_the_Child_Who_Has_the_Ball_After_K_Seconds 문제 알고리즘 구현

---
+++ Gombang/Weekly Contest 401/3178_Find_the_Child_Who_Has_the_Ball_After_K_Seconds.cs
@@ -0,0 +1,23 @@
+﻿public class Solution
+{
+    public int NumberOfChild(int n, int k)
+    {
+        const int RIGHT = 1;
+        const int LEFT = -1;
+
+        int current = 0;
+        int direction = RIGHT;
+
+        for (int i = 0; i < k; i++)
+        {
+            current += direction;
+
+            if (current == n - 1)
+                direction = LEFT;
+            else if (current == 0)
+                direction = RIGHT;
+        }
+
+        return current;
+    }
+}
