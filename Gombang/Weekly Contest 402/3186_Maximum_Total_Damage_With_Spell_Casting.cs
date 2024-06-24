107585b2ed5dc10f61f497765c185b5f484882c4
Kim youngwoo <keiprogramo@gmail.com>
2024-06-24 22:40:20 +0900

    ✨feat : [LTC] 3186_Maximum_Total_Damage_With_Spell_Casting 문제 알고리즘 구현

---
+++ Gombang/Weekly Contest 402/3186_Maximum_Total_Damage_With_Spell_Casting.cs
@@ -0,0 +1,57 @@
+﻿public class Solution
+{
+    public long MaximumTotalDamage(int[] power)
+    {
+        // 빈도수 계산.
+        Dictionary<int, long> damageFrequency = new Dictionary<int, long>();
+        foreach (var damage in power)
+        {
+            if (damageFrequency.ContainsKey(damage))
+            {
+                damageFrequency[damage]++;
+            }
+            else
+            {
+                damageFrequency[damage] = 1;
+            }
+        }
+
+        // damageFrequency의 key값들만 가진 리스트형 변수 생성.
+        List<int> uniqueDamages = damageFrequency.Keys.ToList();
+        uniqueDamages.Sort(); // uniqueDamages 오름차순 정렬.
+
+        long[] maxDamageDP = new long[uniqueDamages.Count];
+
+        maxDamageDP[0] = uniqueDamages[0] * damageFrequency[uniqueDamages[0]];
+
+        // 1번 인덱스부터 dp계산 진행.
+        for (int i = 1; i < uniqueDamages.Count; i++)
+        {
+            int currentDamageValue = uniqueDamages[i];
+            long currentDamageTotal = currentDamageValue * damageFrequency[currentDamageValue];
+
+            maxDamageDP[i] = maxDamageDP[i - 1];
+
+            int previousIndex = i - 1;
+            while (previousIndex >= 0 &&
+                   (uniqueDamages[previousIndex] == currentDamageValue - 1 ||
+                    uniqueDamages[previousIndex] == currentDamageValue - 2 ||
+                    uniqueDamages[previousIndex] == currentDamageValue + 1 ||
+                    uniqueDamages[previousIndex] == currentDamageValue + 2))
+            {
+                previousIndex--;
+            }
+
+            if (previousIndex >= 0)
+            {
+                maxDamageDP[i] = Math.Max(maxDamageDP[i], maxDamageDP[previousIndex] + currentDamageTotal);
+            }
+            else
+            {
+                maxDamageDP[i] = Math.Max(maxDamageDP[i], currentDamageTotal);
+            }
+        }
+
+        return maxDamageDP[uniqueDamages.Count - 1];
+    }
+}
