1c5ab0295666c9147c15c77dd56f6ab78ea1becd
Kim youngwoo <keiprogramo@gmail.com>
2024-06-30 21:18:13 +0900

    ✨feat : [LTC] 3194_Minimum_Average_of_Smallest_and_Largest_Elements 문제 알고리즘 구현

---
+++ Gombang/Weekly Contest 403/3194_Minimum_Average_of_Smallest_and_Largest_Elements.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Solution
+{
+    public double MinimumAverage(int[] nums)
+    {
+        // 오름차순 정렬
+        Array.Sort(nums);
+
+        double result = double.MaxValue;
+        int left = 0;
+        int right = nums.Length - 1;
+
+        while (left < right)
+        {
+            double average = (nums[left] + nums[right]) / 2.0;
+            if (result > average)
+                result = average;
+
+            left++;
+            right--;
+        }
+
+        return result;
+    }
+}
