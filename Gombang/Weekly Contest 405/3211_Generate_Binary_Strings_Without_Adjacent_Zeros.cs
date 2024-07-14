1228e7d5c0bb1822a5b0aa88f2989d5c05a8a39b
Kim youngwoo <keiprogramo@gmail.com>
2024-07-14 16:35:54 +0900

    ✨feat : [LTC] 3211_Generate_Binary_Strings_Without_Adjacent_Zeros 문제 구현

---
+++ Gombang/Weekly Contest 405/3211_Generate_Binary_Strings_Without_Adjacent_Zeros.cs
@@ -0,0 +1,31 @@
+﻿public class Solution
+{
+    public IList<string> ValidStrings(int n)
+    {
+        IList<string> result = new List<string>();
+
+        //0 과 1에 대해서 재귀 호출 시작.
+        AppendNum(n, "0", result);
+        AppendNum(n, "1", result);
+
+        return result;
+    }
+
+    private void AppendNum(int n, string s, IList<string> result)
+    {
+        if (s.Length >= n)
+        {
+            result.Add(s);
+            return;
+        }
+
+        // 마지막 요소가 0이라면 1만 붙을 수 있다.
+        if ($"{s.Last()}" == "0")
+            AppendNum(n, $"{s}1", result);
+        else // 마지막 요소가 1이라면 0과 1이 뒤에 붙을 수 있다.
+        {
+            AppendNum(n, $"{s}0", result);
+            AppendNum(n, $"{s}1", result);
+        }
+    }
+}
