d69041320058eea4e7a4de1d2c91b579125d7e13
hyer0705 <hyer1205@gmail.com>
2024-07-17 06:32:36 +0900

    💡comment: 코드 설명 주석 추가

--- Lucy/Weekly Contest 405/3212_Count_Submatrices_With_Equal_Frequency_of_X_and_Y.js
+++ Lucy/Weekly Contest 405/3212_Count_Submatrices_With_Equal_Frequency_of_X_and_Y.js
@@ -12,15 +12,19 @@
  * @return {number}
  */
 var numberOfSubmatrices = function (grid) {
+  // "X", "Y", "." 문자를 숫자로 치환하는 상수 GRID
   const GRID = Object.freeze({
     X: 1,
     Y: -1,
     ".": 0,
   });
 
+  // submatrix에 최소 하나의 "X"가 있는지 판단하는 함수
   const isAtLeastOneX = (xCnt) => xCnt > 0;
+  // submatrix에 "X"와 "Y"의 빈도수가 동일한지 판단하는 함수
   const isEqualFrequencyXAndY = (xCnt, yCnt) => xCnt - yCnt === 0;
 
+  // 주어진 그리드에 대한 prefixSum 배열을 계산하는 함수
   const calcPrefixSum = (prefixSum, grid) => {
     prefixSum[0][0] = GRID[grid[0][0]];
 
@@ -42,6 +46,7 @@ var numberOfSubmatrices = function (grid) {
       }
     }
   };
+  // 특정 문자의 누적 합(prefix sum) 배열을 계산하는 함수
   const calcSpecificCharPrefixSum = (prefixSum, grid, target) => {
     prefixSum[0][0] = grid[0][0] === target ? 1 : 0;
 
@@ -70,12 +75,16 @@ var numberOfSubmatrices = function (grid) {
     }
   };
 
+  // "X"와 "Y"를 각각 1, -1로 치환하여 누적 합 배열(prefix sum)을 계산하기 위한 배열
   const prefixSum = Array.from({ length: grid.length }, (_) =>
     new Array(grid[0].length).fill(0)
   );
+
+  // "X"의 갯수를 계산하기 위한 누적 합 배열
   const xCntPrefixSum = Array.from({ length: grid.length }, (_) =>
     new Array(grid[0].length).fill(0)
   );
+  // "Y"의 갯수를 계산하기 위한 누적 합 배열
   const yCntPrefixSum = Array.from({ length: grid.length }, (_) =>
     new Array(grid[0].length).fill(0)
   );
@@ -84,6 +93,13 @@ var numberOfSubmatrices = function (grid) {
   calcSpecificCharPrefixSum(xCntPrefixSum, grid, "X");
   calcSpecificCharPrefixSum(yCntPrefixSum, grid, "Y");
 
+  // prefixSum 배열의 요소 중 값이 0인 요소 중에 문제에 나온 submatrix 조건을 만족하는 경우 zeroCnt 변수 count
+  // submatrix 조건
+  // 1. grid[0][0] 포함
+  // 2. an equal frequency of 'X' and 'Y'
+  // 3. at least one 'X'
+
+  // 조건을 만족하는 submatrix의 수를 세기 위한 변수
   let zeroCnt = 0;
   for (let i = 0; i < prefixSum.length; i++) {
     for (let j = 0; j < prefixSum[i].length; j++) {
