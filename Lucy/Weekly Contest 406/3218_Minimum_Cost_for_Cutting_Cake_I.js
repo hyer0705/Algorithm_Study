08f269d8024bcbaeefa8aab46c21720a525f59f8
hyer0705 <hyer1205@gmail.com>
2024-07-21 22:58:30 +0900

    ✏️comment: Minimum Cost for Cutting Cake I 코드 설명 주석 추가

--- Lucy/Weekly Contest 406/3218_Minimum_Cost_for_Cutting_Cake_I.js
+++ Lucy/Weekly Contest 406/3218_Minimum_Cost_for_Cutting_Cake_I.js
@@ -6,6 +6,7 @@
  * date: 2024-07-18(THU)~
  */
 
+// Cut 비용을 클래스로 표현
 class Cut {
   constructor(cost, type) {
     this.cost = cost;
@@ -21,11 +22,13 @@ class Cut {
  * @return {number}
  */
 var minimumCost = function (m, n, horizontalCut, verticalCut) {
+  // Cut의 유형 상수 정의(horizontalCut 배열 요소인지? verticalCut 배열 요소인지?)
   const CUT_TYPE = Object.freeze({
     HORIZONTAL: "h",
     VERTICAL: "v",
   });
 
+  // horizontalCut, verticalCut 배열을 하나의 배열에 관리하기 위한 totalCut 배열
   const totalCut = [];
   horizontalCut.forEach((cost) =>
     totalCut.push(new Cut(cost, CUT_TYPE.HORIZONTAL))
@@ -34,12 +37,16 @@ var minimumCost = function (m, n, horizontalCut, verticalCut) {
     totalCut.push(new Cut(cost, CUT_TYPE.VERTICAL))
   );
 
+  // 내림차순으로 정렬
   totalCut.sort((a, b) => b.cost - a.cost);
 
+  // cutting 비용을 저장할 변수, totalCost
   let totalCost = 0;
+  // cutting 이후 currH - row 갯수, currV - col 갯수
   let currH = 1;
   let currV = 1;
 
+  // totalCut 배열 순회
   for (let i = 0; i < totalCut.length; i++) {
     const currTotalCut = totalCut[i];
     if (currTotalCut.type === CUT_TYPE.HORIZONTAL) {
