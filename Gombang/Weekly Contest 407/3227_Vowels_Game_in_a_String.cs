44619133f86121a87ace6a7026950609e26413cd
Kim youngwoo <keiprogramo@gmail.com>
2024-07-28 18:34:33 +0900

    ✨feat : [LTC] 3227_Vowels_Game_in_a_String 문제 구현

---
+++ Gombang/Weekly Contest 407/3227_Vowels_Game_in_a_String.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+public class Solution
+{
+    public bool DoesAliceWin(string s)
+    {
+        List<char> vowels = new List<char>()
+        {
+            'a', 'e', 'i', 'o', 'u'
+        };
+
+        int vowelCount = 0;
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (vowels.Contains(s[i]))
+                vowelCount++;
+        }
+
+        return vowelCount > 0 ? true : false;
+    }
+}
