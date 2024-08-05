e78e0d811b59cc37b380a128ac64cc8148f4efa6
Kim youngwoo <keiprogramo@gmail.com>
2024-08-05 09:33:12 +0900

    ✨feat : [LTC] 3232_Find_if_Digit_Game_Can_Be_Won 문제 구현

---
+++ Gombang/Weekly Contest 408/3232_Find_if_Digit_Game_Can_Be_Won.cs
@@ -0,0 +1,23 @@
+﻿public class Solution
+{
+	public bool CanAliceWin(int[] nums)
+	{
+
+		int singleDigitSum = 0;
+		int doubleDigitSum = 0;
+
+		for (int i = 0; i < nums.Length; i++)
+		{
+			if (nums[i] < 10)
+			{
+				singleDigitSum += nums[i];
+			}
+			else
+			{
+				doubleDigitSum += nums[i];
+			}
+		}
+
+		return singleDigitSum != doubleDigitSum;
+	}
+}
