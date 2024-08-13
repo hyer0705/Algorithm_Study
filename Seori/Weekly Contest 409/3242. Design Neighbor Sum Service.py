f3e227cffdbd93cdf53a6c6b131c16dc7bcbf587
Seori15 <wlstn6278@gmail.com>
2024-08-13 22:24:32 +0900

    ✨feat : [LTC] 3242_Design Neighbor Sum Service 문제 알고리즘 구현

---
+++ Seori/Weekly Contest 409/3242. Design Neighbor Sum Service.py
@@ -0,0 +1,58 @@
+class neighborSum:
+
+    def __init__(self, grid: List[List[int]]):
+        self.grid = grid
+
+    def adjacentSum(self, value: int) -> int:
+        grid = self.grid
+        i, j = 0, 0
+        length = len(grid)
+        for x in range(length):
+            for y in range(length):
+                if grid[x][y] == value:
+                    i, j = x, y
+        
+        adjacentSum = 0
+        if i - 1 >= 0:
+            adjacentSum += grid[i-1][j]
+
+        if j - 1 >= 0:
+            adjacentSum += grid[i][j-1]
+
+        if i + 1 < length:
+            adjacentSum += grid[i+1][j]
+
+        if j + 1 < length:
+            adjacentSum += grid[i][j+1]
+
+        return adjacentSum
+
+    def diagonalSum(self, value: int) -> int:
+        grid = self.grid
+        i, j = 0, 0
+        length = len(grid)
+        for x in range(length):
+            for y in range(length):
+                if grid[x][y] == value:
+                    i, j = x, y
+        
+        diagonalSum = 0
+        if i - 1 >= 0 and j - 1 >= 0:
+            diagonalSum += grid[i-1][j-1]
+
+        if j - 1 >= 0 and i + 1 < length:
+            diagonalSum += grid[i+1][j-1]
+
+        if i + 1 < length and j + 1 < length:
+            diagonalSum += grid[i+1][j+1]
+
+        if j + 1 < length and i - 1 >= 0:
+            diagonalSum += grid[i-1][j+1]
+
+        return diagonalSum
+
+
+# Your neighborSum object will be instantiated and called as such:
+# obj = neighborSum(grid)
+# param_1 = obj.adjacentSum(value)
+# param_2 = obj.diagonalSum(value)
