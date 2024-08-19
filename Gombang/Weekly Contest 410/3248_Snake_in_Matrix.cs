dfce5a86065a29780ba04af17096cdd17296fd67
Kim youngwoo <keiprogramo@gmail.com>
2024-08-19 21:33:00 +0900

    ✨feat : [LTC] 3248_Snake_in_Matrix 문제 구현

---
+++ Gombang/Weekly Contest 410/3248_Snake_in_Matrix.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+public class Solution
+{
+	public int FinalPositionOfSnake(int n, IList<string> commands)
+	{
+		Dictionary<string, int> commandDic = new Dictionary<string, int>() {
+			{ "UP", -n },
+			{ "DOWN", n },
+			{ "LEFT", -1 },
+			{ "RIGHT", 1}
+		};
+
+		int[] array = new int[n * n];
+		int result = 0;
+
+		foreach (string command in commands)
+		{
+			result += commandDic[command];
+		}
+
+		return result;
+	}
+}
