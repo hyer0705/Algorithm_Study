f3e27d8f8a27a7a3b390f68198d9b8f39f587b56
Kim youngwoo <keiprogramo@gmail.com>
2024-08-24 17:21:13 +0900

    ✨feat : [LTC] 3258_Count_Substrings_That_Satisfy_K-Constraint_I 문제 구현

---
+++ Gombang/Weekly Contest 411/3258_Count_Substrings_That_Satisfy_K-Constraint_I.cs
@@ -0,0 +1,31 @@
+﻿public class Solution
+{
+	public int CountKConstraintSubstrings(string s, int k)
+	{
+		int result = 0;
+		int leftIndex = 0;
+
+		while (leftIndex < s.Length)
+		{
+			int zeroCnt = 0;
+			int oneCnt = 0;
+
+			for (int i = leftIndex; i < s.Length; i++)
+			{
+				if (s[i] == '0')
+					zeroCnt++;
+				else
+					oneCnt++;
+
+				if (zeroCnt <= k || oneCnt <= k)
+					result++;
+				else
+					break;
+			}
+
+			leftIndex++;
+		}
+
+		return result;
+	}
+}
