794c913809e41d7426de3c207e8b2456b8c04c84
Kim youngwoo <keiprogramo@gmail.com>
2024-09-01 23:32:04 +0900

    ✨feat : [LTC] 3264_Final_Array_State_After_K_Multiplication_Operations_I 문제 구현

---
+++ Gombang/Weekly Contest 412/3264_Final_Array_State_After_K_Multiplication_Operations_I.cs
@@ -0,0 +1,21 @@
+﻿public class Solution
+{
+	public int[] GetFinalState(int[] nums, int k, int multiplier)
+	{
+		for (int i = 0; i < k; i++)
+		{
+			int minIndex = 0;
+
+			// 가장 작은 값의 인덱스를 찾는 과정 진행
+			for (int j = 1; j < nums.Length; j++)
+			{
+				if (nums[j] < nums[minIndex])
+					minIndex = j;
+			}
+
+			nums[minIndex] *= multiplier;
+		}
+
+		return nums;
+	}
+}
