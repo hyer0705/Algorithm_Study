8844361ca6f541d4c2f3bc2f271adefc6361f65c
Kim youngwoo <keiprogramo@gmail.com>
2024-09-08 12:46:30 +0900

    ✨feat : [LTC] 3274_Check_if_Two_Chessboard_Squares_Have_the_Same_Color 문제 구현

---
+++ Gombang/Weekly Contest 413/3274_Check_if_Two_Chessboard_Squares_Have_the_Same_Color.cs
@@ -0,0 +1,12 @@
+﻿// 체스판의 문자는 아스키코드 값으로 표현될 수 있기에, '아스키코드 값'과 '숫자'의 합을 통해서
+// 두 수의 합이 짝수면 검은색 발판이고, 홀수면 하얀색 발판이라는 개념을 적용하여 문제를 풀이하였습니다.
+public class Solution
+{
+	public bool CheckTwoChessboards(string coordinate1, string coordinate2)
+	{
+		bool firstIsBlack = (coordinate1[0] + coordinate1[1]) % 2 == 0;
+		bool secondIsBlack = (coordinate2[0] + coordinate2[1]) % 2 == 0;
+
+		return firstIsBlack == secondIsBlack;
+	}
+}
