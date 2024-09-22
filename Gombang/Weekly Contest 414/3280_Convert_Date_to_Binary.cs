79723192790264d66ef1b733bab5acdea15ee3bd
Kim youngwoo <keiprogramo@gmail.com>
2024-09-22 15:37:42 +0900

    ✨feat : [LTC] 3280_Convert_Date_to_Binary 문제 구현

---
+++ Gombang/Weekly Contest 414/3280_Convert_Date_to_Binary.cs
@@ -0,0 +1,18 @@
+﻿// C# 에서 제공하는 Convert.ToString 메소드를 사용하여 2진수 표현을 진행하였다.
+// Convert.ToString 메소드의 첫 번째 인자는 대상이 되는 값이며, 두 번째 인자는 '진수'에 대한 값이다.
+
+public class Solution
+{
+	public string ConvertDateToBinary(string date)
+	{
+		string[] splitedDateArray = date.Split('-');
+
+		for (int i = 0; i < splitedDateArray.Length; i++)
+		{
+			int number = int.Parse(splitedDateArray[i]);
+			splitedDateArray[i] = Convert.ToString(number, 2);
+		}
+
+		return string.Join("-", splitedDateArray); ;
+	}
+}
