ae004d322acb20c2d432c34f7456f7fdf9951456
조혜린 <hyer1205@gmail.com>
2025-02-20 13:09:13 +0900

    ✨feat : [LTC] 2130. Maximum Twin Sum of a Linked List 문제 풀이(모범답안 참고)

--- Lucy/2025/2-W03/2130_Maximum_Twin_Sum_of_a_Linked_List.ts
+++ Lucy/2025/2-W03/2130_Maximum_Twin_Sum_of_a_Linked_List.ts
@@ -10,6 +10,37 @@
  * }
  */
 
+// 두번째 풀이(모범답안 참고)
+function pairSum(head: ListNode | null): number {
+  let slow: ListNode | null = head;
+  let fast: ListNode | null = head;
+
+  while (fast && fast.next) {
+    fast = fast.next.next;
+    slow = slow.next;
+  }
+
+  let prev: ListNode | null = null;
+  while (slow) {
+    const next = slow.next;
+    slow.next = prev;
+    prev = slow;
+    slow = next;
+  }
+
+  let maxTwinSum = -Infinity;
+  let left: ListNode | null = head;
+  let right: ListNode | null = prev;
+  while (right) {
+    maxTwinSum = Math.max(maxTwinSum, left.val + right.val);
+    left = left.next;
+    right = right.next;
+  }
+
+  return maxTwinSum;
+}
+
+// 첫 번째 풀이
 function pairSum(head: ListNode | null): number {
   if (head === null) return 0;
 
