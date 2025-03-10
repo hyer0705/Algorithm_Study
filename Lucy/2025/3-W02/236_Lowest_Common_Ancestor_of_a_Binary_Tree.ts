afb57420b499da66b42b514633267a37a42d88a5
조혜린 <hyer1205@gmail.com>
2025-03-10 22:07:24 +0900

    ✨feat : [LTC] 236. Lowest Common Ancestor of a Binary Tree 문제 풀이

---
+++ Lucy/2025/3-W02/236_Lowest_Common_Ancestor_of_a_Binary_Tree.ts
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function lowestCommonAncestor(root: TreeNode | null, p: TreeNode | null, q: TreeNode | null): TreeNode | null {
+  const dfs = (node: TreeNode | null, target: TreeNode | null, ancestors: TreeNode[]): boolean => {
+    if (node === null) return false;
+
+    ancestors.push(node);
+
+    if (node.val === target.val) return true;
+
+    if (dfs(node.left, target, ancestors) || dfs(node.right, target, ancestors)) return true;
+
+    ancestors.pop();
+
+    return false;
+  };
+
+  const pAncestors: TreeNode[] = [];
+  const qAncestors: TreeNode[] = [];
+
+  dfs(root, p, pAncestors);
+  dfs(root, q, qAncestors);
+
+  const lowestCommonAncestor: TreeNode | null = pAncestors.filter((ancestor) => qAncestors.includes(ancestor)).at(-1);
+
+  return lowestCommonAncestor;
+}
