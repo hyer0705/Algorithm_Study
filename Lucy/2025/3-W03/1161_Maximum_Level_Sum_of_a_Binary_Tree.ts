e3667a3580993cf3f27e327d9aa5c227ffa35dfc
Lucy <hyer1205@gmail.com>
2025-03-17 23:51:13 +0900

    ✨feat : [LTC] 1161. Maximum Level Sum of a Binary Tree 문제 풀이

---
+++ Lucy/2025/3-W03/1161_Maximum_Level_Sum_of_a_Binary_Tree.ts
@@ -0,0 +1,77 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+// 두 번째 풀이: BFS(모범답안) + level 한 번에 찾기
+
+function maxLevelSum(root: TreeNode | null): number {
+  let queue = [root];
+
+  let maxSum = -Infinity;
+  let minLevel = 0;
+  let level = 0;
+
+  while (queue.length) {
+    const tempQueue: TreeNode[] = [];
+    let sum = 0;
+
+    for (let i = 0; i < queue.length; i++) {
+      const node = queue[i];
+
+      if (node.left) tempQueue.push(node.left);
+      if (node.right) tempQueue.push(node.right);
+
+      sum += node.val;
+    }
+
+    queue = tempQueue;
+    level++;
+
+    if (sum > maxSum) {
+      maxSum = sum;
+      minLevel = level;
+    }
+  }
+
+  return minLevel;
+}
+
+// 첫 번째 풀이: BFS + 포인터 + level 분리해서 찾기
+function maxLevelSum(root: TreeNode | null): number {
+  const queue: [TreeNode | null, number][] = [];
+  queue.push([root, 0]);
+
+  let front = 0;
+
+  const levelSums: number[] = [];
+  while (front < queue.length) {
+    const [node, level] = queue[front++];
+
+    if (!node) continue;
+
+    if (level <= levelSums.length && !levelSums[level]) levelSums.push(0);
+
+    levelSums[level] += node.val;
+
+    if (node.left) queue.push([node.left, level + 1]);
+    if (node.right) queue.push([node.right, level + 1]);
+  }
+
+  const maxSums = Math.max(...levelSums);
+  for (let i = 0; i < levelSums.length; i++) {
+    const currentSums = levelSums[i];
+    if (maxSums === currentSums) return i + 1;
+  }
+
+  return -1;
+}
