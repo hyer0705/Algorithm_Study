5c5fa017d6aba26a752165978f3f2e4b70757537
Lucy <hyer1205@gmail.com>
2025-04-09 22:25:43 +0900

    ♻️ Refactor: diff -> imosBoard로 수정

--- Lucy/2025/4-W02/파괴되지_않은_건물.js
+++ Lucy/2025/4-W02/파괴되지_않은_건물.js
@@ -8,36 +8,36 @@ function solution(board, skill) {
 
   const ROW = board.length;
   const COL = board[0].length;
-  const diff = Array.from({ length: ROW + 1 }, () => new Array(COL + 1).fill(0));
+  const imosBoard = Array.from({ length: ROW + 1 }, () => new Array(COL + 1).fill(0));
 
   for (const [type, startRow, startCol, endRow, endCol, degree] of skill) {
     let val = 1;
     if (type === TYPE.enemy) val = -1;
     else val = 1;
 
-    diff[startRow][startCol] += degree * val;
-    diff[startRow][endCol + 1] -= degree * val;
-    diff[endRow + 1][startCol] -= degree * val;
-    diff[endRow + 1][endCol + 1] += degree * val;
+    imosBoard[startRow][startCol] += degree * val;
+    imosBoard[startRow][endCol + 1] -= degree * val;
+    imosBoard[endRow + 1][startCol] -= degree * val;
+    imosBoard[endRow + 1][endCol + 1] += degree * val;
   }
 
   // 행 누적합
   for (let i = 0; i < ROW + 1; i++) {
     for (let j = 1; j < COL + 1; j++) {
-      diff[i][j] += diff[i][j - 1];
+      imosBoard[i][j] += imosBoard[i][j - 1];
     }
   }
 
   // 열 누적합
   for (let j = 0; j < COL + 1; j++) {
     for (let i = 1; i < ROW + 1; i++) {
-      diff[i][j] += diff[i - 1][j];
+      imosBoard[i][j] += imosBoard[i - 1][j];
     }
   }
 
   for (let i = 0; i < ROW; i++) {
     for (let j = 0; j < COL; j++) {
-      const durability = board[i][j] + diff[i][j];
+      const durability = board[i][j] + imosBoard[i][j];
       if (durability > 0) answer++;
     }
   }
